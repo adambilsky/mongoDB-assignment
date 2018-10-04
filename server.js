b736b647d59678300a271ae1784dddeb7d588950
Adam Bilsky <adambilsky@gmail.com>
2018-10-04 17:01:15 -0500

    server (routes) update

--- server.js
+++ server.js
@@ -41,16 +41,16 @@ mongoose.connect("mongodb://localhost/scraperdb", {
 // Routes
 
 // A GET route for scraping the ... site
-app.get("/scrape", function(req, res) {
+app.get("/scrape", function (req, res) {
     // get body of html
-    .then(function(response) {
-    //changed result to response
+    .then(function (response) {
+        //changed result to response
         // pass result to cheerio
         var $ = cheerio.load(response.data);
 
         // then grab specific element tags (we are using 'a' to grab link titles) and save as object
         // we use ".each()" instead of a "for loop" 
-        $("").each(function(i, element) {
+        $("").each(function (i, element) {
             // start with an empty object
             var result = {};
 
@@ -66,51 +66,74 @@ app.get("/scrape", function(req, res) {
 
             // NOW, we create a new Article using the "result" object above just built, using the models
             db.Article.create(result)
-                .then(function(dbArticle) {
+                .then(function (dbArticle) {
                     console.log(dbArticle);
                 })
-                .catch(function(err) {
+                .catch(function (err) {
                     // send errors back to client
                     return res.json(err);
                 });
         });
-        
+
         // Display if successful
         res.send("Scrape successful");
     });
 });
 
 // Next, a route for getting all the articles in the scraperdb
+app.get("/articles", function (req, res) {
 
     // First get all the documents in the Articles "collection"
+    db.Article.findOneAndUpdate({})
+        .then(function (dbArticle) {
+            // If successful, send them back to the client
+            res.json(dbArticle);
+        })
+        // Send any errors that occurred
+        .catch(function (err) {
+            res.json(err);
+        });
+})
 
-    // If successful, send them back to the client
-
-    // Send any errors that occurred
 
 // Route for Articles by id to include the note
+app.get("/articles/:id", function (req, res) {
 
-    // Build a query using the id param to match the one in scraperdb
-
-      // use the "populate" method to include all notes
-
-        // Send back a successful get to the client
-
+    // Build a query using the id param and findOne method to match the one in scraperdb
+    db.Article.findOne({ _id: req.params.id })
+        // use the "populate" method to include all notes
+        .populate("note")
+        .then(function (dbArticle) {
+            // Send back a successful get to the client
+            res.json(dbArticle);
+        })
         // Send back any errors
-
+        .catch(function (err) {
+            res.json(err);
+        });
+});
 
 // Route for saving/updating notes (use the specific id as the route)
+app.post("/articles/:id", function (req, res) {
 
-  // Create a new note and pass the body
+    // Create a new note and pass the body
+    db.Note.create(req.body)
+        .then(function (dbNote) {
 
-    // Since the query normally returns the original id, we need to update it with a { new: true } param using findOneAndUpdate
-    
-    // Once we update/find the new Article, send it to the client
-    
-    // Send back any errors
-
+            // Since the query normally returns the original id, we need to update it with a { new: true } param using findOneAndUpdate
+            return db.Article.findOneAndUpdate({ _id: requ.params.id }, { note: dbNote._id }, { new: true });
+        })
+        // Once we update/find the new Article, send it to the client
+        .then(function (dbArticle) {
+            res.json(dbArticle);
+        })
+        // Send back any errors
+        .catch(function (err) {
+            res.json(err);
+        });
+});
 
 // Start the server
-app.listen(PORT, function() {
+app.listen(PORT, function () {
     console.log(`App running on port ${PORT}!`)
 })
